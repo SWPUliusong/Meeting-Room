7d7f3771c80729ddf6b094c51c8a9ded90d6ff01
SWPUliusong <swpuliusong@foxmail.com>
2021-05-15 16:08:15 +0800

    清理文件结构

--- webrtc-signal-server/src/services/index.ts
+++ webrtc-signal-server/src/services/index.ts
@@ -1,13 +1,3 @@
-// const userServices = require("./user")
-// const rtcServices = require("./rtc")
-// const meetingServices = require("./meeting")
-
 export * from "./user"
 export * from "./rtc"
 export * from "./meeting"
-
-// module.exports = {
-//   ...userServices,
-//   ...rtcServices,
-//   ...meetingServices,
-// }
